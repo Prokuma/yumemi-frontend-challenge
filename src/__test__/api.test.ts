ed9c5ce6bc38368b168717e2ac441864e6a75eb8
Prokuma <prokuma@prokuma.dev>
2024-01-29 19:18:11 +0900

    [Update] #2 変換関数追加

--- src/__test__/api.test.ts
+++ src/__test__/api.test.ts
@@ -1,28 +1,129 @@
 /**
-* @jest-environment jsdom　　　　
-*/
-import 'cross-fetch/polyfill'
-import { getPrefectures, getPopulationComposition } from '../api'
-import setupEnv from './setupEnv'
+ * @jest-environment jsdom
+ */
+import "cross-fetch/polyfill";
+import {
+    getPrefectures,
+    getPopulationComposition,
+    convertPopulationCompositionToChartDataset,
+} from "../api";
+import setupEnv from "./setupEnv";
 
-describe('getPrefectures', () => {
-    it('should return prefectures', async () => {
-        await setupEnv()
-        const prefectures = await getPrefectures()
-        expect(prefectures.length).toBeGreaterThan(0)
-    })
-})
+describe("getPrefectures", () => {
+    it("should return prefectures", async () => {
+        await setupEnv();
+        const prefectures = await getPrefectures();
+        expect(prefectures.length).toBeGreaterThan(0);
+    });
+});
 
-describe('getPopulationComposition', () => {
-    it('should return population composition', async () => {
-        await setupEnv()
+describe("getPopulationComposition", () => {
+    it("should return population composition", async () => {
+        await setupEnv();
         const populationComposition = await getPopulationComposition({
             prefCode: 1,
-            cityCode: '-'
-        })
-        console.log(populationComposition)
-        console.log(populationComposition.data)
-        console.log(populationComposition.data[0].data)
-        expect(populationComposition.data.length).toBeGreaterThan(0)
-    })
-})
+            cityCode: "-",
+        });
+        expect(populationComposition.data.length).toBeGreaterThan(0);
+        expect(populationComposition.data[0].data.length).toBeGreaterThan(0);
+    });
+});
+
+describe("convertPopulationCompositionToChartDataset", () => {
+    const populationCompositions = [
+        {
+            boundaryYear: 2015,
+            data: [
+                {
+                    label: "総人口",
+                    data: [
+                        {
+                            year: 2015,
+                            value: 100,
+                        },
+                    ],
+                },
+                {
+                    label: "年少人口",
+                    data: [
+                        {
+                            year: 2015,
+                            value: 100,
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            boundaryYear: 2015,
+            data: [
+                {
+                    label: "総人口",
+                    data: [
+                        {
+                            year: 2015,
+                            value: 100,
+                        },
+                    ],
+                },
+                {
+                    label: "年少人口",
+                    data: [
+                        {
+                            year: 2015,
+                            value: 100,
+                        },
+                    ],
+                },
+            ],
+        },
+    ];
+    it("should return chart dataset", () => {
+        const prefectures = [
+            {
+                code: 1,
+                name: "北海道",
+            },
+            {
+                code: 2,
+                name: "青森県",
+            },
+        ];
+        const datasets = convertPopulationCompositionToChartDataset(
+            populationCompositions,
+            prefectures
+        );
+        expect(datasets).toEqual({
+            総人口: [
+                {
+                    year: 2015,
+                    北海道: 100,
+                    青森県: 100,
+                },
+            ],
+            年少人口: [
+                {
+                    year: 2015,
+                    北海道: 100,
+                    青森県: 100,
+                },
+            ],
+        });
+    });
+    it("should throw an error", () => {
+        const prefectures = [
+            {
+                code: 1,
+                name: "北海道",
+            },
+        ];
+        expect(() => {
+            convertPopulationCompositionToChartDataset(
+                populationCompositions,
+                prefectures
+            );
+        }).toThrowError(
+            "The length of populationCompositions and prefectures must be the same."
+        );
+    });
+});
--- src/api.ts
+++ src/api.ts
@@ -47,3 +47,40 @@ export async function getPopulationComposition(request: APIRequestPopulation) {
         return {} as APIPopulation;
     }
 }
+
+export function convertPopulationCompositionToChartDataset(
+    populationCompositions: APIPopulation[],
+    prefectures: APIPrefecture[]
+) {
+    var datasets: { [key: string]: any } = {};
+    if (!populationCompositions || !prefectures) {
+        return datasets;
+    } else if (populationCompositions.length !== prefectures.length) {
+        throw new Error(
+            "The length of populationCompositions and prefectures must be the same."
+        );
+    }
+    for (const i in populationCompositions) {
+        const prefName = prefectures[i].name;
+
+        for (const data of populationCompositions[i].data) {
+            const label = data.label;
+            if (!datasets[label]) {
+                datasets[label] = {};
+            }
+
+            for (const value of data.data) {
+                if (!datasets[label][value.year]) {
+                    datasets[label][value.year] = {};
+                    datasets[label][value.year]["year"] = value.year;
+                }
+                datasets[label][value.year][prefName] = value.value;
+            }
+        }
+    }
+    for (const label in datasets) {
+        datasets[label] = Object.values(datasets[label]);
+    }
+
+    return datasets;
+}
